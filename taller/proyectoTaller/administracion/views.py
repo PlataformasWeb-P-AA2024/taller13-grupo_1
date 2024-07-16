dc06042effc9dc3f30a825c83d3c333c7b6be758
Maiscal <maiscal1999@gmail.com>
2024-07-15 19:38:21 -0500

    v2

--- taller/proyectoTaller/administracion/views.py
+++ taller/proyectoTaller/administracion/views.py
@@ -3,11 +3,27 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render
 
+# importar para login
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# importar django-rest
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from administrativo.serializers import UserSerializer, GroupSerializer, \
+EdificioSerializer, DepartamentoSerializer
+
 # importar las clases de models.py
 from administrativo.models import *
 
 # importar los formularios de forms.py
 from administrativo.forms import *
+
+
+
 
 def index(request):
     edificio = Edificio.objects.all()
@@ -22,6 +38,35 @@ def obtener_edificio(request, id):
         'edificio': edificio
     }
     return render(request, 'obtener_edificio.html', diccionario)
+
+
+
+## ---------------- ingresar y salir sesion
+
+def ingreso(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        print(form.errors)
+        if form.is_valid():
+            username = form.data.get("username")
+            raw_password = form.data.get("password")
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                return redirect(index)
+    else:
+        form = AuthenticationForm()
+
+    informacion_template = {'form': form}
+    return render(request, 'registration/login.html', informacion_template)
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Has salido del sistema")
+    return redirect(index)
+
 
 
 
@@ -52,6 +97,9 @@ def crear_departamento(request):
 
     return render(request, 'crear_departamento.html', diccionario)
 
+
+
+
 ## ---------------- editar
 def editar_edificio(request, id):
     edificio = Edificio.objects.get(pk=id)
@@ -81,6 +129,9 @@ def editar_departamento(request, id):
 
     return render(request, 'editar_departamento.html', diccionario)   
 
+
+
+
 ## ---------------- eliminar
 def eliminar_edificio(request, id):
     edificio = Edificio.objects.get(pk=id)
@@ -91,3 +142,32 @@ def eliminar_departamento(request, id):
     departamento = Departamento.objects.get(pk=id)
     departamento.delete()
     return redirect(index)
+
+
+
+
+
+
+## -------vistas a traves de viewsets
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class EdificioViewSet(viewsets.ModelViewSet):
+    queryset = Edificio.objects.all()
+    serializer_class = EdificioSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DepartamentoViewSet(viewsets.ModelViewSet):
+    queryset = Departamento.objects.all()
+    serializer_class = DepartamentoSerializer
+    permission_classes = [permissions.IsAuthenticated]
